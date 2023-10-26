438a06e5ed17c779be4db6ee938625ed1d4ab11e
Alexius Huang <alexius.huang@gmail.com>
2023-10-27 00:57:11 +0200

    Update

--- src/stacks/generate_parentheses.rs
+++ src/stacks/generate_parentheses.rs
@@ -6,7 +6,7 @@
  *  combinations of well-formed parentheses.
  */
 
-use std::collections::HashMap;
+use std::{collections::HashMap, thread, time::{Instant, Duration}};
 
 pub fn run(n: i32) -> Vec<String> {
     let first_term = vec!["()".to_owned()];
@@ -103,5 +103,159 @@ mod test {
             "(()())()()", "(((())))()", "(()(()))()",
             "((()()))()", "(()()())()", "((())())()"
         ]));
+    }
+}
+
+// Solution 2 based on one crucial rule: closing parentheses must
+// be more than opening parentheses
+
+struct Parentheses {
+    result: String,
+    remaining_open: u32,
+    remaining_close: u32
+}
+
+impl Parentheses {
+    fn new(remaining: u32) -> Self {
+        Self {
+            result: "(".to_owned(),
+            remaining_open: remaining - 1,
+            remaining_close: remaining
+        }
+    }
+
+    fn push_opening(&self) -> Self {
+        Self {
+            result: format!("{}(", self.result),
+            remaining_open: self.remaining_open - 1,
+            ..*self
+        }
+    }
+
+    fn push_closing(&self) -> Self {
+        Self {
+            result: format!("{})", self.result),
+            remaining_close: self.remaining_close - 1,
+            ..*self
+        }
+    }
+
+    fn is_closable(&self) -> bool {
+        self.remaining_close > self.remaining_open
+    }
+
+    fn finalize(&self) -> String {
+        format!(
+            "{}{}",
+            self.result,
+            ")".repeat(self.remaining_close as usize).as_str()
+        )
+    }
+}
+
+pub fn solution_2(n: i32) -> Vec<String> {
+    fn parse_parentheses(p: Parentheses) -> Vec<String> {
+        if p.remaining_open == 0 {
+            return vec![
+                if p.remaining_close == 0 { p.result }
+                else { p.finalize() }
+            ]
+        }
+
+        let mut result = parse_parentheses(p.push_opening());
+
+        if p.is_closable() {
+            result.append(&mut parse_parentheses(p.push_closing()));
+        }
+
+        result
+    }
+
+    parse_parentheses(
+        Parentheses::new(n as u32)
+    )
+}
+
+pub fn solution_3(n: i32) -> Vec<String> {
+    fn parse_parentheses(p: Parentheses) -> Vec<String> {
+        if p.remaining_open == 0 {
+            return vec![
+                if p.remaining_close == 0 { p.result }
+                else { p.finalize() }
+            ]
+        }
+
+        if !p.is_closable() {
+            return parse_parentheses(p.push_opening());
+        }
+
+        let mut opening_result = vec![];
+        let mut closing_result = vec![];
+
+        thread::scope(|scope| {
+            scope.spawn(|| {
+                opening_result.append(&mut parse_parentheses(p.push_opening()));
+            });
+
+            scope.spawn(|| {
+                closing_result.append(&mut parse_parentheses(p.push_closing()));
+            });
+        });
+
+        opening_result.append(&mut closing_result);
+
+        opening_result
+    }
+
+    parse_parentheses(
+        Parentheses::new(n as u32)
+    )
+}
+
+// After experiment, thread version isn't faster than without thread
+pub fn run_comparison(loop_time: i32, max_gen: i32) {
+    println!("Running {loop_time} times gen_parens 1 to {max_gen} without thread");
+
+    let mut result_without_thread: Vec<Duration> = vec![];
+
+    for i in 1..=max_gen {
+        let now = Instant::now();
+        for _ in 0..loop_time {
+            solution_2(i);
+        }
+        result_without_thread.push(Instant::now() - now);
+
+        println!("Without thread: gen({i}) complete!");
+    }
+
+    println!("Running {loop_time} times gen_parens 1 to {max_gen} with thread");
+
+    let mut result_with_thread: Vec<Duration> = vec![];
+
+    for i in 1..=max_gen {
+        let now = Instant::now();
+        for _ in 0..loop_time {
+            solution_3(i);
+        }
+        result_with_thread.push(Instant::now() - now);
+
+        println!("With thread: gen({i}) complete!");
+    }
+
+    println!("Without Thread v.s. With Thread");
+    
+    for i in 1..=max_gen {
+        let without_thread = result_without_thread[i as usize - 1];
+        let with_thread = result_with_thread[i as usize - 1];
+        println!(
+            "gen({i}) {:?} v.s. {:?} => {} wins!",
+            without_thread,
+            with_thread,
+            if with_thread < without_thread {
+                "With Thread"
+            } else {
+                "Without Thread"
+            }
+        );
     }
 }
