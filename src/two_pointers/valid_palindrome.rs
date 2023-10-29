4ff9c35efbf20126b815fe07fb3694ea09cb3704
Alexius Huang <alexius.huang@gmail.com>
2023-10-29 17:12:51 +0100

    Update palindrome with valid two pointer algorithm

--- src/two_pointers/valid_palindrome.rs
+++ src/two_pointers/valid_palindrome.rs
@@ -38,6 +38,49 @@ pub fn run(s: String) -> bool {
     forward_vec == backward_vec
 }
 
+pub fn run_with_no_additional_memory_required(s: String) -> bool {
+    let s = s.trim();
+    if s.len() < 2 { return true; }
+
+    let chars = s.chars().collect::<Vec<char>>();
+    let mut left_index = 0;
+    let mut right_index = s.len() - 1;
+    
+    while right_index != left_index {
+        let mut left_char = chars[left_index];
+        match left_char {
+            'a'..='z' | 'A'..='Z' => {
+                left_char = left_char.to_ascii_lowercase();
+            },
+            '0'..='9' => (),
+            _ => {
+                left_index += 1;
+                continue;    
+            }
+        }
+
+        let mut right_char = chars[right_index];
+        match right_char {
+            'a'..='z' | 'A'..='Z' => {
+                right_char = right_char.to_ascii_lowercase();
+            },
+            '0'..='9' => (),
+            _ => {
+                right_index -= 1;
+                continue;
+            }
+        }
+
+        if left_char != right_char { return false; }
+        if right_index - left_index == 1 { return true; }
+
+        left_index += 1;
+        right_index -= 1;
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,5 +94,17 @@ mod test {
         assert!(!run("race a car".to_owned()));
         assert!(!run("1234567890".to_owned()));
         assert!(!run("abc012".to_owned()));
+    }
+
+    #[test]
+    fn it_validates_if_the_string_is_palindrome_by_using_two_pointer_algorithm() {
+        assert!(run_with_no_additional_memory_required("A man, a plan, a canal: Panama".to_owned()));
+        assert!(run_with_no_additional_memory_required("12321".to_owned()));
+        assert!(run_with_no_additional_memory_required(" ".to_owned()));
+
+        assert!(!run_with_no_additional_memory_required("race a car".to_owned()));
+        assert!(!run_with_no_additional_memory_required("1234567890".to_owned()));
+        assert!(!run_with_no_additional_memory_required("abc012".to_owned()));
+        assert!(!run_with_no_additional_memory_required("0P".to_owned()));
     }
 }
